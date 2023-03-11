dca686f0dae66ebd181ed0ba880613c513062dec
Wondersteve254 <106595167+Wondersteve254@users.noreply.github.com>
2023-03-11 12:04:44 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function animateBars() {
+    const bars = document.querySelectorAll('.skill-bar-progress');
+    bars.forEach(bar => {
+      const value = bar.dataset.value;
+      bar.style.width = value + '%';
+    });
+  }
+  
+  window.onload = animateBars;
+  window.onresize = animateBars;
+{
+    const button = document.getElementById('myButton');
+    button.addEventListener('click', function() {
+      console.log('Button clicked!');
+    });
+}
